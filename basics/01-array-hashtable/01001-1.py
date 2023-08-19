08b5caca7f757c0b73c99d1da639212f63f7c5b8
un01s <96022876+un01s@users.noreply.github.com>
2023-08-19 15:17:42 -0600

    update

--- basics/01-array-hashtable/01001-1.py
+++ basics/01-array-hashtable/01001-1.py
@@ -40,3 +40,11 @@ class Solution2:
             if target-n in nums[i+1:]:
                 return [i, i+1+nums[i+1:].index(target-n)]
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i, n in enumerate(nums):
+            if n in dict:
+                return [dict[n], i]
+            else:
+                dict[target-n] = i
