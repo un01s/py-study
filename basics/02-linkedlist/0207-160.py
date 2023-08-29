73e6207cfd0865495d0aed2fd8c3ee3936d55446
un01s <96022876+un01s@users.noreply.github.com>
2023-08-29 10:47:07 -0600

    update

---
+++ basics/02-linkedlist/0207-160.py
@@ -0,0 +1,17 @@
+"""
+leetcode 160 in python3
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA and headB:
+            p1, p2 = headA, headB
+            while p1 != p2:
+                p1 = p1.next if p1 else headB
+                p2 = p2.next if p2 else headA
+            return p1
