193424423d3d791e71201bd611656de02346cd93
un01s <96022876+un01s@users.noreply.github.com>
2023-09-09 20:24:04 -0600

    update

---
+++ basics/02-linkedlist/0209-203.cpp
@@ -0,0 +1,26 @@
+/*
+ * leetcode 203
+ *
+ */
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if(cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;        
+    }
+};
+
