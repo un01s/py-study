ce2b5d46f564579d887a64edec2b339caac0164c
un01s <96022876+un01s@users.noreply.github.com>
2023-09-13 20:19:25 -0600

    update

---
+++ basics/01-array/0129-54.cpp
@@ -0,0 +1,37 @@
+/*
+ * leetcode 54
+ * 
+ */
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector <int> ans;
+        if(matrix.empty()) return ans;
+
+        int u = 0;
+        int d = matrix.size() - 1;
+        int l = 0;
+        int r = matrix[0].size() - 1;
+        while(true)
+        {
+            for(int i = l; i <= r; ++i) 
+                ans.push_back(matrix[u][i]);
+            if(++ u > d) break;
+            
+            for(int i = u; i <= d; ++i) 
+                ans.push_back(matrix[i][r]);
+            if(-- r < l) break;
+        
+            for(int i = r; i >= l; --i) 
+                ans.push_back(matrix[d][i]);
+            if(-- d < u) break;
+        
+            for(int i = d; i >= u; --i) 
+                ans.push_back(matrix[i][l]);
+            if(++ l > r) break;
+        }
+
+        return ans;   
+    }
+};
