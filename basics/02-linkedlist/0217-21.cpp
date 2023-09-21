20813582b1d5c5fa012151f316ca77cfe40116d4
un01s <96022876+un01s@users.noreply.github.com>
2023-09-21 10:11:45 -0600

    update

---
+++ basics/02-linkedlist/0217-21.cpp
@@ -0,0 +1,32 @@
+/*
+ * leetcode 21
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* p = dummy;
+
+        while(p1 != nullptr && p2 != nullptr) {
+            if (p1->val > p2->val) {
+                p->next = p2;
+                p2 = p2->next;
+            } else {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            p = p->next;
+        }
+        if (p1 != nullptr) {
+            p->next = p1;
+        }
+        if (p2 != nullptr) {
+            p->next = p2;
+        }
+        return dummy->next;
+    }
+};
+
