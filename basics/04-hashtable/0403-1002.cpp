1c3baa9b30c88fa1fcbf81b403c3f1557ebd6f0e
un01s <96022876+un01s@users.noreply.github.com>
2023-09-27 19:36:53 -0600

    update

--- basics/04-hashtable/0403-1002.cpp
+++ basics/04-hashtable/0403-1002.cpp
@@ -24,3 +24,26 @@ public:
         return res;        
     }
 };
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<string> res;
+        vector<int> minFreq(26, INT_MAX);
+        for (auto& w: words) {
+            vector<int> freq(26, 0);
+            for (char c: w) {
+                freq[c-'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = min(minFreq[i], freq[i]);
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                res.push_back(string(1, i + 'a'));
+            }
+        }
+        return res;        
+    }
+};
