0636927cb8e8b775115316af1a704d790fb1feb5
un01s <96022876+un01s@users.noreply.github.com>
2023-10-02 18:39:54 -0600

    update

--- basics/01-array/0137-1512.cpp
+++ basics/01-array/0137-1512.cpp
@@ -21,3 +21,14 @@ public:
     }
 };
 
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            res += mp[nums[i]]++;
+        }
+        return res;    
+    }
+};
